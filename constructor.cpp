42ee8c19f1e0fc555c6ac31927c5effe3d3e16d0
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-07-23 18:33:12 +0530

    Create constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Student
+
+{
+
+   public:
+
+   string name;
+
+   int roll_no;
+
+
+   Student()
+
+   {
+
+       name = "John";
+
+       roll_no = 2;
+
+   }
+
+};
+
+
+int main() {
+
+
+   Student a;
+
+   cout<<a.name<<endl;
+
+   cout<<a.roll_no<<endl;
+
+
+
+
+   return 0;
+
+}
